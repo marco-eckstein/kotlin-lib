41004a238240a18d15e70cfe2b66a26a1bdeb754
Marco Eckstein <marco.eckstein@gmx.de>
2021-03-06 01:04:47 +0100

    feat: rename BigDecimal extension functions
    BREAKING CHANGE: rename BigDecimal extension functions

--- src/jvmMain/kotlin/com/marcoeckstein/klib/java/math/BigDecimalExtensions.kt
+++ src/jvmMain/kotlin/com/marcoeckstein/klib/java/math/BigDecimalExtensions.kt
@@ -2,8 +2,8 @@ package com.marcoeckstein.klib.java.math
 
 import java.math.BigDecimal
 
-infix fun BigDecimal.equalsComparingTo(other: BigDecimal) =
+infix fun BigDecimal.equalsComparing(other: BigDecimal) =
     compareTo(other) == 0
 
-infix fun BigDecimal.notEqualsComparingTo(other: BigDecimal) =
+infix fun BigDecimal.notEqualsComparing(other: BigDecimal) =
     compareTo(other) != 0
